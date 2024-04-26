75c1f6f95db31c3e08d9a1d867384d071b07746d
Felipe Castillo <felcastillo3@hotmail.com>
2024-04-26 10:46:37 -0300

    cambios felipe 26-04

---
+++ Mingle/js/registro.js
@@ -0,0 +1,19 @@
+// Ejemplo de JavaScript para deshabilitar el envío de formularios si hay campos no válidos
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()	 
